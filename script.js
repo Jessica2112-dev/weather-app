2af58b471d9058c276fe45f86c121e44871727d6
Jessica Gorai <goraijessica439@gmail.com>
2025-12-29 10:24:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const apikey="df69df875dbfe4a72f5cdd151309d210";
+async function getWeather(){
+    const city=document.getElementById("cityInput").value;
+    const resultDiv=document.getElementById("result");
+    if(city==""){
+        resultDiv.innerHTML="Please enter a city name";
+        return;
+    }
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+    try{
+        const response=await fetch(url);
+        if(!response.ok){
+            resultDiv.innerHTML="City not found.";
+            return;
+        }
+        // const temp=data.main.temp;
+        // const humidity=data.main.humidity;
+        // const weather=data.weather[0].description;
+        const data=await response.json();
+        const icon=data.weather[0].icon;
+        const iconUrl=`https://openweathermap.org/img/wn/${icon}@2x.png`;
+        const description=data.weather[0].description.charAt(0).toUpperCase()+data.weather[0].description.slice(1);
+        resultDiv.innerHTML=`<h3>${data.name}</h3> 
+                              <img src="${iconUrl}" alt="weather icon">
+                              <p><b>${description}</b></p>
+                              <p>&#127777 Temperature: ${data.main.temp} C</p>
+                              <p>&#128167 Humidity: ${data.main.humidity}%</p>
+                              `;
+    }
+    catch(error){
+        resultDiv.innerHTML=`Oops! Something went wrong.`;
+        console.error(error);
+    }
+}
+document.getElementById("getWeatherBtn").addEventListener("click",getWeather);
+document.getElementById("cityInput").addEventListener("keypress",function(e){
+    if(e.key=="Enter"){
+        getWeather();
+    }
+});
